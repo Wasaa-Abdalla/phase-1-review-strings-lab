970697da69927ca7bb6137da19b1fa6e1f8c92d7
abdalla-wasaa <abdallawasaa7@gmail.com>
2023-07-12 00:09:06 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+const currentUser = "Michael Scofield";
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase() + currentUser.toUpperCase() + "!";
+
+const shortGreeting = "Welcome, " + "M" + "!"; 
+
+
+
+
+
+
